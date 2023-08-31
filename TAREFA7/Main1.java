1657b3afcd31cd45aa280ed5acc5d78f909ac05b
nutelinha02 <arthcc@outlook.com>
2023-08-31 15:28:58 +0000

    fazendo a 1

---
+++ TAREFA7/Main1.java
@@ -0,0 +1,13 @@
+package TAREFA7;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Paciente p = new Paciente("0000","Arthur","31/08/2002",'M',"Amil","Poeira","O+");
+        System.out.println(p);
+        p.setNome("Ricardo");
+        p.setDataNascimento("01/11/1963");
+        p.getSexo('M');
+        P.getAlergia(){}
+
+    }
+}
